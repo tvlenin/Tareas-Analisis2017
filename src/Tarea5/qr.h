4bb951ba58326d8670f3e03c60cce61e8a4271d7
Lenin <ttvleninn@gmail.com>
2017-04-17 11:14:30 -0600

    msj

--- src/Tarea5/qr.h
+++ src/Tarea5/qr.h
@@ -2,6 +2,7 @@
 #define _ANPI_QR_H
 #include <vector>
 #include "Matrix.hpp"
+#include <math.h>
 
 
 namespace anpi{
@@ -12,13 +13,18 @@ namespace anpi{
     std::vector<Matrix<T>> qMatrix;
 
   public:
-    qr();
+    qr(){};
     // get the first column of Matrix A
     vector<T> getColumn(const Matrix<T> A, int sizec , int sizer){}
     // get the norm of a vector
-    T getNorm(const vector<T>, T norm){}
-    //Return the identity matrix sizec x sizec
-    Matrix<T> IMatrix(int sizec){}
+    T getNorm(const vector<T> vec){
+      int sizeV = vec.size();
+      T ans = 0;
+      for (int i = 0; i < sizeV;i++){
+        ans += pow(vec[i],2);
+      }
+      return sqrt(ans);
+    }
     //get de diferent qmatrix and put this on qMatrix vector
     void getQMatrix(Matrix<T> IMatrix, vector<T> u){}
 
