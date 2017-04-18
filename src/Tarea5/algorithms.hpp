3c44577bc63f7debbde64a46f34f60818ceb9cb3
Abraham <abrahamon>
2017-04-18 10:26:10 -0600

    testQR

--- src/Tarea5/algorithms.hpp
+++ src/Tarea5/algorithms.hpp
@@ -39,5 +39,11 @@ T algorithms<T>::testLU(Matrix<T>& A, Matrix<T>& L, Matrix<T>& U){
 
 template<typename T>
 T algorithms<T>::testQR(Matrix<T>& A, Matrix<T>& Q, Matrix<T>& R){
-
+	Matrix<double>*ans = new Matrix<double>((*A)-((*L)*(*U)));
+	T err;
+	T* end = (*ans)(0,0) + (ans->_rows*ans->_cols);
+	for (T* ptr = (*ans)(0,0); ptr!=end ;++ptr) {
+		err += (*ans)*(*ans);
+	}
+	return pow(err,1/n);
 }
